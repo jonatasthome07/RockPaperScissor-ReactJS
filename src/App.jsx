f1605163baab154c5c2ab24d5039748304ac9708
Jônatas <jonatasthome04@gmail.com>
2025-08-29 12:09:47 -0300

    Started to apply useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [rock, setRock] = useState()
+  const [paper, setPaper] = useState()
+  const [scissor, setScissor] = useState()
+  const [userPoints, setUserPoints] = useState(0)
+  const [cpuPoints, setCpuPoints] = useState(0)
+  const [match, setMatch] = useState()
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main-container">
+        <h1>Rock, Paper and Scissor</h1>
+        <h2>Your points: {userPoints}</h2>
+        <h2>CPU points: {cpuPoints}</h2>
+
+      <div className="actions">
+        <button>Rock</button>
+        <button>Paper</button>
+        <button>Scissor</button>
+        <button>Reset game</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
